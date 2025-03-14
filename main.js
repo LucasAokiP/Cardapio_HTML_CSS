142b274023c9e33fe9de2e2f183a9b6c70596e49
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-03-14 20:32:40 -0300

    adcionei comentarios em botoesAdicionar e listaPedido

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
     //simulação de adiçao de produtos na cesta do site
-    const botoesAdicionar   = document.querySelectorAll(".adicionar");
-    const listaPedido       = document.getElementById('Lista_Pedido');
+    const botoesAdicionar   = document.querySelectorAll(".adicionar");  //querySelectorAll ele pega todos os elementos da classe
+    const listaPedido       = document.getElementById('Lista_Pedido');  //getElementById pega o elemento pelo ID dele
     const totalElemento     = document.getElementById('total');
     let total               = 0;
 
