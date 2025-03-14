27ea4643a192f68126208bb1dac46ff8a0511f2d
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-03-14 20:30:04 -0300

    criaçao do main.js e adiçao de alguns comandos para os botoes

---
+++ main.js
@@ -0,0 +1,6 @@
+    //simulação de adiçao de produtos na cesta do site
+    const botoesAdicionar   = document.querySelectorAll(".adicionar");
+    const listaPedido       = document.getElementById('Lista_Pedido');
+    const totalElemento     = document.getElementById('total');
+    let total               = 0;
+
