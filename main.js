7b4516bff2d03deda7ff2ddc9cefe16fd5cdc0f1
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-03-14 21:24:01 -0300

    adçao de codigo para pegar os valors dos botoes

--- main.js
+++ main.js
@@ -4,3 +4,18 @@
     const totalElemento     = document.getElementById('total');
     let total               = 0;
 
+    botoesAdicionar.forEach(botao =>
+            {botao.addEventListener('click',() => {
+                //obtem o elemnto pai do botao
+                const produto = botao.parentElement;
+                //obtem o nome do produto a partir do texto dentro do elemento H3
+                const nome = produto.querySelector('h3').textContent;
+                //obtem o preço do produto removendo o texto R$ e convertendo para numero decimal
+                const preco = parseFloat(produto.querySelector('.preco').textContent.replace('R$',''));
+                // cria novo item de lista <li> para adicionar o produto ao pedido
+                const itemPedido = document.createElement('li');
+                // formata o texto com duas casas
+                itemPedido.textContent = `${nome} - R$ ${preco.toFixed(2)}`;
+            });
+   });
+
