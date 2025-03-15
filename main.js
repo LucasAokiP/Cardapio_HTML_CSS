016753ec77dd5619ebf5edcc92c18c76e87b79d1
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-03-14 21:59:31 -0300

    adiçao do codigo finalizar pedido

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
     let total               = 0;
 
     botoesAdicionar.forEach(botao =>
-            {botao.addEventListener('click',() => {
+            {botao.addEventListener('click', () => {
                 //obtem o elemnto pai do botao
                 const produto = botao.parentElement;
                 //obtem o nome do produto a partir do texto dentro do elemento H3
@@ -16,6 +16,24 @@
                 const itemPedido = document.createElement('li');
                 // formata o texto com duas casas
                 itemPedido.textContent = `${nome} - R$ ${preco.toFixed(2)}`;
+                //adciona item criado a lista de pedidos
+                
+                listaPedido.appendChild(itemPedido);
+
+                total += preco;
+
+                totalElemento.textContent =`total: R$ ${total.toFixed(2)}`;
             });
    });
 
+    //simulaçao de finalizaçao do pedido
+    //seleciona o botao "finalizar pedido"
+    const botaoFinalizarPedido = document.getElementById('finalizar_pedido');
+    //adiciona evento de clique no botao finalizar pedido
+
+    botaoFinalizarPedido.addEventListener('click', () => {alert("pedido realizado com sucesso (Simulação)");
+        //para limpar o campo apos a mensagem de sucesso
+        listaPedido.innerHTML = ``;
+        total = 0;
+        totalElemento.textContent = `total: R$ ${total.toFixed(2)}`
+    });
